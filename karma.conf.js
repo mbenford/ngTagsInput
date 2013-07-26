098a983c9007cd0efb606ee62ebfb1d732631c5a
Michael Benford <michael@michaelbenford.net>
2013-07-26 18:20:06 -0300

    Added rule to ignore *.log files

---
+++ karma.conf.js
@@ -0,0 +1,70 @@
+// Karma configuration
+// Generated on Thu Jul 25 2013 19:29:25 GMT-0300 (BRT)
+
+
+// base path, that will be used to resolve files and exclude
+basePath = '';
+
+
+// list of files / patterns to load in the browser
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'test/lib/jquery-1.10.2.min.js',
+  'test/lib/angular.js',
+  'test/lib/angular-mocks.js',
+  'test/ngTagsInput.spec.js',
+  'ngTagsInput.js'
+];
+
+
+// list of files to exclude
+exclude = [
+  
+];
+
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
+
+
+// web server port
+port = 9876;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['Chrome'];
+
+
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 60000;
+
+
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
