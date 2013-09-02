9a5c02519540739e0c3053589bd938af52554e89
Michael Benford <michael@michaelbenford.net>
2013-09-02 01:08:04 -0300

    Added demo, getting started and documentation pages

--- js/app.js
+++ js/app.js
@@ -9,4 +9,23 @@ angular.module("ngTagsInputSite", ['tags-input'])
         $scope.goToGitHub = function() {
             $window.location = 'http://github.com/mbenford/ngTagsInput';
         };
+    })
+    .controller('DemoCtrl', function($scope) {
+        $scope.tags = ['just','some','cool','tags'];
+        $scope.superheroes = ['Batman', 'Superman', 'Flash', 'Iron Man', 'Hulk', 'Wolverine', "Green Lantern", "Green Arrow", "Spiderman"];
+        $scope.movies = ['The Dark Knight',
+            'Heat',
+            'Inception',
+            'The Dark Knight Rises',
+            'Kill Bill: Vol. 1',
+            'Terminator 2: Judgment Day',
+            'The Matrix',
+            'Minority Report',
+            'The Bourne Ultimatum',
+            'Kill Bill: Vol. 2',
+            'Hot Fuzz',
+            'Serpico',
+            'The Fugitive',
+            'Casino Royale',
+            'Ghost Dog: The Way of the Samurai'];
     });
