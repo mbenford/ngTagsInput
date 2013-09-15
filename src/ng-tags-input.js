4f26cb52d705f7e778bac3e7c6c6be8e8359ff5f
Michael Benford <michael@michaelbenford.net>
2013-09-15 01:48:17 -0300

    Added JSDoc-like documentation

--- src/ng-tags-input.js
+++ src/ng-tags-input.js
@@ -1,6 +1,30 @@
 (function() {
 'use strict';
 
+/**
+ * @ngdoc directive
+ * @name tagsInput.directive:tagsInput
+ *
+ * @description
+ * ngTagsInput is an Angular directive that renders an input box with tag editing support.
+ *
+ * @param {string} ngModel Assignable angular expression to data-bind to.
+ * @param {string} ngClass CSS class to style the control.
+ * @param {number} tabindex Tab order of the control.
+ * @param {string=Add a tag} placeholder Placeholder text for the control.
+ * @param {number=3} minLength Minimum length for a new tag.
+ * @param {number=} maxLength Maximum length allowed for a new tag.
+ * @param {string=×} removeTagSymbol Symbol character for the remove tag button.
+ * @param {boolean=true} addOnEnter Flag indicating that a new tag will be added on pressing the ENTER key.
+ * @param {boolean=false} addOnSpace Flag indicating that a new tag will be added on pressing the SPACE key.
+ * @param {boolean=true} addOnComma Flag indicating that a new tag will be added on pressing the COMMA key.
+ * @param {boolean=true} replaceSpacesWithDashes Flag indicating that spaces will be replaced with dashes.
+ * @param {string=^[a-zA-Z0-9\s]+$*} allowedTagsPattern Regular expression that determines whether a new tag is valid.
+ * @param {boolean=false} enableEditingLastTag Flag indicating that the last tag will be moved back into
+ *                                             the new tag input box instead of being removed when the backspace key
+ *                                             is pressed and the input box is empty.
+ */
+
 angular.module('tags-input', []).directive('tagsInput', function() {
     function toBool(value, defaultValue) {
         return angular.isDefined(value) ? value === 'true' : defaultValue;
@@ -12,7 +36,10 @@ angular.module('tags-input', []).directive('tagsInput', function() {
         replace: false,
         template: '<div class="ngTagsInput {{ cssClass }}">' +
                   '  <ul>' +
-                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)"><span>{{ tag }}</span><button type="button" ng-click="remove($index)">{{ removeTagSymbol }}</button></li>' +
+                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)">' +
+                  '      <span>{{ tag }}</span>' +
+                  '      <button type="button" ng-click="remove($index)">{{ removeTagSymbol }}</button>' +
+                  '    </li>' +
                   '  </ul>' +
                   '  <input type="text" placeholder="{{ placeholder }}" size="{{ placeholder.length }}" maxlength="{{ maxLength }}" tabindex="{{ tabindex }}" ng-model="newTag">' +
                   '</div>',
