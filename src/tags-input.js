5c7d82497d7782349e0b5fedfae71e127995df31
runxc1(Bret Ferrier) <runxc1@gmail.com>
2014-02-10 18:26:22 -0700

    Allow dots and dashes by default in tags

--- src/tags-input.js
+++ src/tags-input.js
@@ -73,7 +73,7 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig) 
                 addOnSpace: [Boolean, false],
                 addOnComma: [Boolean, true],
                 addOnBlur: [Boolean, true],
-                allowedTagsPattern: [RegExp, /^[a-zA-Z0-9\s]+$/],
+                allowedTagsPattern: [RegExp, /^[a-zA-Z0-9\s-\.]+$/],
                 enableEditingLastTag: [Boolean, false],
                 minTags: [Number],
                 maxTags: [Number]
