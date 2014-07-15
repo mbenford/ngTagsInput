606cecc98118b06618a78f40b8b132399c9d09dd
Michael Benford <michael@michaelbenford.net>
2014-07-15 12:21:22 -0300

    chore(grunt): Set up Saucelabs task
    Set up a new task to run all tests on Saucelabs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.loadTasks('grunt/tasks');
 
-    grunt.registerTask('test', ['jshint','karma']);
+    grunt.registerTask('test', ['jshint','karma:local']);
     grunt.registerTask('travis', ['test', 'coveralls']);
     grunt.registerTask('coverage', ['test', 'open:coverage']);
 
--- grunt/options/karma.js
+++ grunt/options/karma.js
@@ -1,10 +1,23 @@
-module.exports = {
-    options: {
-        configFile: 'karma.conf.js'
-    },
-    continuous: {
-        singleRun: true,
-        browsers: ['PhantomJS'],
-        reporters: ['progress', 'coverage']
-    }
+'use strict';
+
+module.exports = function(grunt) {
+    return {
+        options: {
+            configFile: 'karma.conf.js'
+        },
+        local: {
+            singleRun: true,
+            browsers: ['PhantomJS'],
+            reporters: ['progress', 'coverage']
+        },
+        remote: {
+            singleRun: true,
+            captureTimeout: 120000,
+            sauceLabs: {
+                testName: 'ngTagsInput'
+            },
+            customLaunchers: grunt.file.readJSON('sauce.launchers.json'),
+            reporters: ['progress', 'saucelabs']
+        }
+    };
 };
