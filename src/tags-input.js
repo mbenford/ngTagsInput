7e0235bd280bf4c64c14cc3f6978ad3ce888cbc1
se7 <se7@users.noreply.github.com>
2014-12-22 15:53:11 -0800

    fix(tagsInput): Only updating model through ng-model
    Not setting tags explicitly as that circumvents ng-model and can create invalid models on parent, especially when model hasn't been loaded yet (tags === undefined). Removed test for testing just that and defaulted .tags to be [] in tests for them to work the way they were written. Also defaulted to updating BOTH through ngModel and direct 2-way angular binding on tags, since otherwise a lot of tests would have broken.
    
    Can break code that relies on their model being initialized indirectly by tagsInput. Closes #320
    
    Conflicts:
    	src/tags-input.js

--- src/tags-input.js
+++ src/tags-input.js
@@ -206,8 +206,9 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig, 
                 setElementValidity;
 
             setElementValidity = function() {
-                ngModelCtrl.$setValidity('maxTags', scope.tags.length <= options.maxTags);
-                ngModelCtrl.$setValidity('minTags', scope.tags.length >= options.minTags);
+                var tagsLength = (scope.tags && scope.tags.length) || 0;
+                ngModelCtrl.$setValidity('maxTags', tagsLength <= options.maxTags);
+                ngModelCtrl.$setValidity('minTags', tagsLength >= options.minTags);
                 ngModelCtrl.$setValidity('leftoverText', scope.hasFocus || options.allowLeftoverText ? true : !scope.newTag.text);
             };
 
@@ -229,8 +230,7 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig, 
             };
 
             scope.$watch('tags', function(value) {
-                scope.tags = tiUtil.makeObjectArray(value, options.displayProperty);
-                tagList.items = scope.tags;
+                tagList.items = tiUtil.makeObjectArray(value, options.displayProperty);
             });
 
             scope.$watch('tags.length', function() {
