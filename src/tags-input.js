e22197c936c7e30877d9a26eb961af7de8288c8f
se7 <se7@users.noreply.github.com>
2014-12-22 15:53:11 -0800

    fix(tagsInput): Only updating model through ng-model
    Not setting tags explicitly as that circumvents ng-model and can create invalid models on parent, especially when model hasn't been loaded yet (tags === undefined). Removed test for testing just that and defaulted .tags to be [] in tests for them to work the way they were written. Also defaulted to updating BOTH through ngModel and direct 2-way angular binding on tags, since otherwise a lot of tests would have broken.
    
    Can break code that relies on their model being initialized indirectly by tagsInput. Closes #320

--- src/tags-input.js
+++ src/tags-input.js
@@ -193,8 +193,9 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig) 
                 setElementValidity;
 
             setElementValidity = function() {
-                ngModelCtrl.$setValidity('maxTags', scope.tags.length <= options.maxTags);
-                ngModelCtrl.$setValidity('minTags', scope.tags.length >= options.minTags);
+                var tagsLength = (scope.tags && scope.tags.length) || 0;
+                ngModelCtrl.$setValidity('maxTags', tagsLength <= options.maxTags);
+                ngModelCtrl.$setValidity('minTags', tagsLength >= options.minTags);
                 ngModelCtrl.$setValidity('leftoverText', options.allowLeftoverText ? true : !scope.newTag.text);
             };
 
@@ -209,8 +210,7 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig) 
             };
 
             scope.$watch('tags', function(value) {
-                scope.tags = makeObjectArray(value, options.displayProperty);
-                tagList.items = scope.tags;
+                tagList.items = makeObjectArray(value, options.displayProperty);
             });
 
             scope.$watch('tags.length', function() {
@@ -266,7 +266,8 @@ tagsInput.directive('tagsInput', function($timeout, $document, tagsInputConfig) 
                 .on('tag-added tag-removed', function() {
                     // Sets the element to its dirty state
                     // In Angular 1.3 this will be replaced with $setDirty.
-                    ngModelCtrl.$setViewValue(scope.tags);
+                    scope.tags = tagList.items;
+                    ngModelCtrl.$setViewValue(tagList.items);
                 })
                 .on('invalid-tag', function() {
                     scope.newTag.invalid = true;
