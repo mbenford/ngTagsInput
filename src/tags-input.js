fa3dad4bdb771b3809cf4513bfc478249d948cd7
Jesús Carrera <jesus.carrera@frontendmatters.com>
2015-05-12 13:20:03 +0200

    docs(ngTagsInput): add inputId to the param list

--- src/tags-input.js
+++ src/tags-input.js
@@ -15,6 +15,7 @@
  * @param {string=} [type=text] Type of the input element. Only 'text', 'email' and 'url' are supported values.
  * @param {number=} tabindex Tab order of the control.
  * @param {string=} [placeholder=Add a tag] Placeholder text for the control.
+ * @param {string=} [inputId=NA] Sets the input id.
  * @param {number=} [minLength=3] Minimum length for a new tag.
  * @param {number=} [maxLength=MAX_SAFE_INTEGER] Maximum length allowed for a new tag.
  * @param {number=} [minTags=0] Sets minTags validation error key if the number of tags added is less than minTags.
