064cb41e0f2e22022aa867b8c31585bb98b7912b
Maia Naftali <maia@poincenot.com>
2015-07-22 10:07:13 -0300

    a11y improvements: adding aria-label to tag input using the placeholder, and the ability to hook an id to use a label

--- src/tags-input.js
+++ src/tags-input.js
@@ -170,6 +170,7 @@ tagsInput.directive('tagsInput', function($timeout, $document, $window, tagsInpu
                 template: [String, 'ngTagsInput/tag-item.html'],
                 type: [String, 'text', validateType],
                 placeholder: [String, 'Add a tag'],
+                id:[String, ''],
                 tabindex: [Number, null],
                 removeTagSymbol: [String, String.fromCharCode(215)],
                 replaceSpacesWithDashes: [Boolean, true],
