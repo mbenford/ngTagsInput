f8bacb9e6e476d6ae51af36eafbd1428855c8921
Jozef Hartinger <jharting@redhat.com>
2016-04-22 18:19:36 +0200

    Make ng-tags work with jquery 1.8
    Workaround for https://github.com/mbenford/ngTagsInput/issues/405

--- src/bind-attrs.js
+++ src/bind-attrs.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global console*/
 
 /**
  * @ngdoc directive
@@ -12,7 +13,15 @@ tagsInput.directive('tiBindAttrs', function() {
     return function(scope, element, attrs) {
         scope.$watch(attrs.tiBindAttrs, function(value) {
             angular.forEach(value, function(value, key) {
-                attrs.$set(key, value);
+                try {
+                    attrs.$set(key, value);
+                } catch (error) {
+                    // workaround for jQuery 1.8 (https://bugs.jquery.com/ticket/13011)
+                    // see https://github.com/mbenford/ngTagsInput/issues/405
+                    if (console && console.error) {
+                        console.error('Error while setting ' + key + ' to ' + value + ': ' + error.message);
+                    }
+                }
             });
         }, true);
     };
